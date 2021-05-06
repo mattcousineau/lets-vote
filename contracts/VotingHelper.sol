c6cc18ed47391da56b954fb0d7231fba28615d3b
mjcousineau1@gmail.com <mjcousineau1@gmail.com>
2021-05-06 17:08:29 -0400

    filled out more test

--- contracts/VotingHelper.sol
+++ contracts/VotingHelper.sol
@@ -1,13 +1,4 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-contract VotingHelper {
-    function _alreadyRegistered(address candidateAddress)
-        internal
-        pure
-        returns (bool)
-    {
-        //TODO:
-        return false;
-    }
-}
+contract VotingHelper {}
